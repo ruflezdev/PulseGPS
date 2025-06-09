5b446bca7e8055e3c015ce3a01e700e6accf0818
ruflezdev <rubenlomeli2807@outlook.com>
2025-06-08 23:53:40 -0600

    Primer commit: versión funcional que lee datos de Firebase y muestra ubicación en mapa

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFxl7PAmnaUckuBIpvMIuY24q_NtW8MJA",
+  authDomain: "pulsegps.firebaseapp.com",
+  databaseURL: "https://pulsegps-default-rtdb.firebaseio.com",
+  projectId: "pulsegps",
+  storageBucket: "pulsegps.firebasestorage.app",
+  messagingSenderId: "190543823804",
+  appId: "1:190543823804:web:6b369d5af6ca25d6a761b9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
